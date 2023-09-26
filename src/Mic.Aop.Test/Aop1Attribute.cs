28ffa83c44daeb6c0e5a57d8ccf326e351628806
maomingbo <766131065@qq.com>
2023-09-26 12:09:00 +0800

    修改readme

--- src/Mic.Aop.Test/Aop1Attribute.cs
+++ src/Mic.Aop.Test/Aop1Attribute.cs
@@ -5,9 +5,11 @@
     /// </summary>
     public class Aop1Attribute : AopInterceptor
     {
-        /// <summary>执行前操作，异步方法调用</summary>
-        /// <param name="context"></param>
-        /// <returns></returns>
+        /// <summary>
+        /// 定义一个参数
+        /// </summary>
+        public string Prop1 { get; set; }
+
         public override ValueTask<AopContext> BeforeAsync(AopContext context)
         {
             Console.WriteLine("BeforeAsync action...");
@@ -15,9 +17,6 @@
             return base.BeforeAsync(context);
         }
 
-        /// <summary>执行后操作，异步方法调用</summary>
-        /// <param name="context"></param>
-        /// <returns></returns>
         public override ValueTask<AopContext> AfterAsync(AopContext context)
         {
             Console.WriteLine("AfterAsync action...");
@@ -34,12 +33,10 @@
         /// 在分析器 - SourceGenerator.Template.Generators 下面可以看到生成的代码，名称是 AopExtend_SampleService_g
         /// 生成了 SampleService 的子类，注入的时候，请注入子类
         /// </summary>
-        [Aop1(AopTag = true)]
+        [Aop1(AopTag = true, Prop1 = "传一个值进去")]
         public virtual void TestAsync()
         {
             Console.WriteLine("Test action...");
-
-            //new SampleService_g().Test();
         }
     }
 }
